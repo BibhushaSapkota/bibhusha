bfe6e7565b3834072702901fdf009cec4944fe05
Bibhushasapkota <Bibhushasapkota5@gmail.com>
2021-02-16 11:37:42 +0545

    slight changes

---
+++ bibhusha.py
@@ -0,0 +1,6 @@
+# program to calculate the perimeter of the rectangle
+length=float(input('enter the length'))
+breadth=float(input('enter the breadth'))
+perimeter= 2*(length+breadth)
+print('the perimeter of rectangle is ', perimeter,'cm')
+
